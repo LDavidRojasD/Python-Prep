77db866a353563772ceea20990dd7ac95ea3ec6c
LDavidRojasD <ldavidrd@gmail.com>
2023-08-09 11:27:39 -0500

    checkpoint commit

--- Simulación_Challenge/checkpoint.py
+++ Simulación_Challenge/checkpoint.py
@@ -11,8 +11,15 @@ def Factorial(numero):
         Factorial(4) debe retornar 24
         Factorial(-2) debe retornar nulo
     '''
-    #Tu código aca:
-    return 'Funcion incompleta'
+    if type(numero)!=int:
+        return None
+    if numero <1:
+        return None
+    resultado=1
+    for i in range(1,numero+1):
+        resultado*=i
+
+    return resultado
 
 def EsPrimo(valor):
     '''
@@ -25,8 +32,15 @@ def EsPrimo(valor):
         EsPrimo(7) debe retornar True
         EsPrimo(8) debe retornar False
     '''
-    #Tu código aca:
-    return 'Funcion incompleta'
+    if type(valor)!=int:
+        return None
+    Primo=True
+    for i in range(2,valor):
+        if valor%i==0:
+            Primo=False
+            break
+    
+    return Primo
     
 def ClaseAnimal(especie, color):
     '''
@@ -47,4 +61,13 @@ def ClaseAnimal(especie, color):
         a.CumpliAnios() -> debe devolver 3
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    class Animal:
+        def __init__(self,especie, color):
+            self.edad=0
+            self.especie=especie
+            self.color=color
+        def CumplirAnios(self):
+            self.edad+=1
+            return self.edad
+            
+    return Animal (especie,color)
